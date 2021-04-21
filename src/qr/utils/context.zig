550b10cca70145c3b8da5ea4fc8157fc69559e69
gernest <geofreyernest@live.com>
2021-04-21 22:39:57 +0300

    add tests for Context

--- src/qr/utils/context.zig
+++ src/qr/utils/context.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 
 const Context = struct {
     heap: *std.mem.Allocator,
@@ -18,4 +19,24 @@ const Context = struct {
     pub fn deinit(self: Context) void {
         self.arena.deinit();
     }
+
+    pub fn gai(self: *Context, comptime T: type, n: usize, zero: T) ![]T {
+        var a = try self.ga().alloc(T, n);
+        var i: usize = 0;
+        while (i < a.len) : (i += 1) {
+            a[i] = zero;
+        }
+        return a;
+    }
 };
+
+test "initialize zero" {
+    var ctx = Context.init(std.testing.allocator);
+    defer ctx.deinit();
+
+    var one = try ctx.gai(u8, 1, 0);
+    testing.expectEqual(one[0], 0);
+
+    var two = try ctx.gai(u8, 2, 2);
+    testing.expectEqual(two[1], 2);
+}
