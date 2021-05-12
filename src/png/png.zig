68b16a84019c316256216f155471111dceb8f84b
gernest <geofreyernest@live.com>
2021-05-12 18:01:11 +0300

    png: define top level stuff

--- src/png/png.zig
+++ src/png/png.zig
@@ -47,3 +47,88 @@ fn filterPaeth(cdat: []u8, cdat: []u8, bytes_per_pixel: usize) void {
         }
     }
 }
+
+const ColorType = enum {
+    Grayscale = 0,
+    TrueColor = 2,
+    Paletted = 3,
+    GrayscaleAlpha = 4,
+    TrueColorAlpha = 6,
+};
+
+const ColorDepth = enum {
+    Invalid,
+    G1,
+    G2,
+    G4,
+    G8,
+    GA8,
+    TC8,
+    P1,
+    P2,
+    P4,
+    P8,
+    TCA8,
+    G16,
+    GA16,
+    TC16,
+    TCA16,
+
+    fn palleted(v: ColorDepth) bool {
+        const x = @enumToInt(ColorDepth{.P1});
+        const y = @enumToInt(ColorDepth{.P8});
+        const z = @enumToInt(v);
+        return x <= z and z <= y;
+    }
+};
+
+const Filter = enum {
+    None,
+    Sub,
+    Up,
+    Average,
+    Paeth,
+    NFilter,
+};
+
+const Interlace = enum {
+    None,
+    Adam7,
+};
+
+const interlaceScan = struct {
+    xf: isize,
+    yf: isize,
+    xo: isize,
+    yo: isize,
+
+    fn init(xf: isize, yf: isize, xo: isize, yo: isize) interlaceScan {
+        return .{
+            .xf = xf,
+            .yf = yf,
+            .xo = xo,
+            .yo = yo,
+        };
+    }
+};
+
+const interlacing = [_]interlaceScan{
+    interlaceScan.init(8, 8, 0, 0),
+    interlaceScan.init(8, 8, 4, 0),
+    interlaceScan.init(4, 8, 0, 4),
+    interlaceScan.init(4, 4, 2, 0),
+    interlaceScan.init(2, 4, 0, 2),
+    interlaceScan.init(2, 2, 1, 0),
+    interlaceScan.init(1, 2, 0, 1),
+};
+
+const DecodingStage = enum {
+    Start,
+    SeenIHDR,
+    SeenPLTE,
+    SeentRNS,
+    SeenIDAT,
+    SeenIEND,
+};
+
+const png_header = "\x89PNG\r\n\x1a\n";
